9072e9c377f3b43e16d0675e40989942a81d8f78
tzhabska <tanya.zhabskaya@gmail.com>
2026-07-25 20:45:00 -0700

    Update spark_session.py

--- spark_jobs/spark_session.py
+++ spark_jobs/spark_session.py
@@ -1,1 +1,16 @@
+from pyspark.sql import SparkSession
 
+
+def create_spark_session():
+    """
+    Creates and returns Spark session for the data pipeline.
+    """
+
+    spark = (
+        SparkSession.builder
+        .appName("NYC_Taxi_Data_Pipeline")
+        .config("spark.sql.shuffle.partitions", "200")
+        .getOrCreate()
+    )
+
+    return spark
