46cf378ce616cff72d4dc76330a881ad640d0077
tzhabska <tanya.zhabskaya@gmail.com>
2026-07-25 20:39:03 -0700

    Update logger.py

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,1 +1,23 @@
+import logging
 
+
+def get_logger(name: str):
+    """
+    Creates and returns a project logger.
+    """
+
+    logger = logging.getLogger(name)
+
+    if not logger.handlers:
+        handler = logging.StreamHandler()
+
+        formatter = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
+
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+        logger.setLevel(logging.INFO)
+
+    return logger
